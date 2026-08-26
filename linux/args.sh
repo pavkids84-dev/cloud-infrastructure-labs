1d277a39fd6fc1df13e323ca11729845bce63da6
pavkids84-dev <235221059+pavkids84-dev@users.noreply.github.com>
2026-08-26 14:25:40 +0900

    Create args.sh

---
+++ linux/args.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Display command-line arguments passed to this script.
+
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Argument count: $#"
+echo "All arguments: $@"
